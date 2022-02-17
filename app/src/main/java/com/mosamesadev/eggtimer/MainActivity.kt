77763ad9166d3d55e806703d134c409e8336cbed
Hendrik Brouwer <h.brouwer@gmail.com>
2022-02-17 14:25:15 +0100

    1st

---
+++ app/src/main/java/com/mosamesadev/eggtimer/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.mosamesadev.eggtimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Layout manager
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val item = ArrayList<User>()
+
+        //adding user, the name for this variables User, Firstname and Lastname are inherited from one of the tutorials I have followed. I wont change this name, in honor of them :)
+
+        item.add(User("https://raw.githubusercontent.com/MosaMesaDev/myCV/master/src/main/res/drawable/mycvastrologer.png","Astrologer", "The Astrologer activity was a basic lesson for me about working with input fields in an activity. Even tho in this particular case, the outcome will always be the same ;) ."))
+        item.add(User("https://raw.githubusercontent.com/MosaMesaDev/myCV/master/src/main/res/drawable/mycvbestproperty.png","Property locator", "In this activity you want to compare 5 neighbourhoods, you want to know which has the lowest max distance to the 5 services you need. \n\nFor interactivity, you place 5 important services, and then the algorithm will show which area has the lowest maximum distance to all 5 services."))
+
+        //Adapter setting
+        val newAdapter = RecyclerViewAdapter(item)
+        recyclerView.adapter = newAdapter
+        newAdapter.setOnItemClickListener(object : RecyclerViewAdapter.onItemClickListener{
+            override fun onItemClick(position: Int) {
+
+
+                //If you click on position 0, you open the Astrologer activity
+                if( position == 0 )                     {
+                    val intent = android.content.Intent(this@MainActivity, Astrologer::class.java)
+                    startActivity(intent)
+                }
+
+            }
+
+        })
+
+
+    }
+}
---
+++ app/src/main/java/com/mosamesadev/eggtimer/RecyclerViewAdapter.kt
@@ -0,0 +1,62 @@
+package com.mosamesadev.eggtimer
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class RecyclerViewAdapter(val userList: ArrayList<User>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+
+    private lateinit var mListener : onItemClickListener
+
+    interface onItemClickListener{
+
+        fun onItemClick(position: Int)
+
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener){
+
+        mListener = listener
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ViewHolder(view, mListener)
+    }
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bind(userList[position])
+    }
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+    class ViewHolder(itemView: View, listener:onItemClickListener) : RecyclerView.ViewHolder(itemView) {
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+
+        }
+        fun bind(user: User) {
+
+            Glide.with(itemView.context)
+                .load(user.CVRecyclerImage)
+                .into(itemView.findViewById(R.id.CVRecyclerImage))
+
+            val textViewName = itemView.findViewById<TextView>(R.id.firstname)
+            val textViewLastname = itemView.findViewById<TextView>(R.id.lastname)
+
+            textViewName.text = user.Firstname
+            textViewLastname.text = user.Lastname
+
+
+        }
+    }
+}
