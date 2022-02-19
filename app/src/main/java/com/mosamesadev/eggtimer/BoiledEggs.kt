4cae4e396331d6e08c0da70279a58c5d90d879f6
Hendrik Brouwer <h.brouwer@gmail.com>
2022-02-19 20:43:28 +0100

    Tried fragments, but did not work. The different buttons on BoiledEggs now pass a different value of seconds for the Timer to start off with. This should also work for future activities that will link to the timer.
    Everything looks still very alpha, this is not yet a concern.

--- app/src/main/java/com/mosamesadev/eggtimer/BoiledEggs.kt
+++ app/src/main/java/com/mosamesadev/eggtimer/BoiledEggs.kt
@@ -1,18 +1,59 @@
 package com.mosamesadev.eggtimer
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_boiledeggs.*
 
 class BoiledEggs : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_boiledeggs)
+        var secs = 0
 
         btn4minutetimer.setOnClickListener {
-            val intent = android.content.Intent(this, Timer::class.java)
+
+            val intent = Intent(this, Timer::class.java)
+                secs = 240
+                intent.putExtra("secs", secs)
+            startActivity(intent)
+            }
+        btn5minutetimer.setOnClickListener {
+
+            val intent = Intent(this, Timer::class.java)
+            secs = 300
+            intent.putExtra("secs", secs)
             startActivity(intent)
         }
-    }
+        btn6minutetimer.setOnClickListener {
 
-}
+            val intent = Intent(this, Timer::class.java)
+            secs = 360
+            intent.putExtra("secs", secs)
+            startActivity(intent)
+        }
+        btn7minutetimer.setOnClickListener {
+
+            val intent = Intent(this, Timer::class.java)
+            secs = 420
+            intent.putExtra("secs", secs)
+            startActivity(intent)
+        }
+        btn8minutetimer.setOnClickListener {
+
+            val intent = Intent(this, Timer::class.java)
+            secs = 480
+            intent.putExtra("secs", secs)
+            startActivity(intent)
+        }
+        btn10minutetimer.setOnClickListener {
+
+            val intent = Intent(this, Timer::class.java)
+            secs = 600
+            intent.putExtra("secs", secs)
+            startActivity(intent)
+        }
+
+        }
+    }
--- app/src/main/java/com/mosamesadev/eggtimer/timer.kt
+++ app/src/main/java/com/mosamesadev/eggtimer/timer.kt
@@ -7,78 +7,81 @@ import android.os.CountDownTimer
 import android.view.View
 
 class Timer : AppCompatActivity() {
-    private var progr = 240
-    val start = 240_000L
-    var timer = start
-    lateinit var countDownTimer: CountDownTimer
 
+    // initial values and types for the progress circle and timer counter
+    private var timeprogressstart = 0
+    var timeprogress = 0
+    private var start = 0L
+    var timer = 0L
+    lateinit var countDownTimer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer)
 
+        // gets the number of seconds to start with, from the previous activity
+        val intent = intent
+        val numberOfSeconds = intent.getIntExtra("secs", 100)
 
+        // Turns numberOfSeconds into millis and into type Long
+        start = (numberOfSeconds*1000).toLong()
+        // sets up where the timer starts from
+        timer = start
+        // sets up where the progress circle starts from
+        timeprogressstart = numberOfSeconds
+        timeprogress = timeprogressstart
+        // sets the max value of the progress circle to the number of seconds that will be counted down
+        progress_bar.max = timeprogressstart
 
-//      the first value
-                setTextTimer()
+        setTextTimer()
         updateProgressBar()
 
-            }
+    }
 
-            fun on(view: View) {
-                when(view.id){
-                    R.id.btn_main_start -> startTimer()
-                    R.id.btn_main_pause -> pauseTimer()
-                    R.id.btn_main_rest -> resetTimer()
-                }
-            }
+    fun on(view: View) {
+        when(view.id){
+            R.id.btn_start -> startTimer()
+            R.id.btn_pause -> pauseTimer()
+            R.id.btn_reset -> resetTimer()
+        }
+    }
 
-            //    btn start
-            private fun startTimer() {
-                countDownTimer = object : CountDownTimer(timer,1000){
-                    //            end of timer
-
-                    override fun onTick(millisUntilFinished: Long) {
-                        timer = millisUntilFinished
-                        setTextTimer()
-
-                        progr = (timer/1000).toInt()
-                        updateProgressBar()
-                    }
-
-                    override fun onFinish() {
-                        TODO("Not yet implemented")
-                    }
-
-                }.start()
-            }
-
-            //    btn pause
-            private fun pauseTimer() {
-                countDownTimer.cancel()
-            }
-
-            //    btn restart
-            private fun resetTimer() {
-                countDownTimer.cancel()
-                timer = start
-                progr = 240
+    private fun startTimer() {
+        countDownTimer = object : CountDownTimer(timer,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                timer = millisUntilFinished
                 setTextTimer()
+                timeprogress = (timer/1000).toInt()
                 updateProgressBar()
             }
 
-            //  timer format
-            fun setTextTimer() {
-                var m = (timer / 1000) / 60
-                var s = (timer / 1000) % 60
-
-                var format = String.format("%02d:%02d", m, s)
-
-                tv_main_timer.setText(format)
+            override fun onFinish() {
+                TODO("Not yet implemented")
             }
-
-
-    private fun updateProgressBar() {
-        progress_bar.progress = progr
+        }.start()
     }
+
+    private fun pauseTimer() {countDownTimer.cancel()}
+
+    private fun resetTimer() {
+        countDownTimer.cancel()
+        timer = start
+        timeprogress = timeprogressstart
+        setTextTimer()
+        updateProgressBar()
+    }
+
+    fun setTextTimer() {
+        var m = (timer / 1000) / 60
+        var s = (timer / 1000) % 60
+
+        var format = String.format("%02d:%02d", m, s)
+
+        tv_main_timer.text = format
+    }
+
+    fun updateProgressBar() {progress_bar.progress = timeprogress}
+
 }
+
+
